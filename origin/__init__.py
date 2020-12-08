6ef6f70837337919e64376719a02cdd29c9fd5f8
deathbybandaid <github@deathbybandaid.net>
2020-12-08 13:45:59 -0500

    Initial Template Push

---
+++ origin/__init__.py
@@ -0,0 +1,7 @@
+# pylama:ignore=W0401,W0611
+from .origin_service import *
+from .origin_channels import *
+from .origin_epg import *
+
+ORIGIN_NAME = "fHDHR_TEMPLATE"
+ORIGIN_VERSION = "v0.5.0-beta"
